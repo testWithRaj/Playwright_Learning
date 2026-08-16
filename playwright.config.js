b874267b9b0226c2fe92f3c79e84dd02ce820728
rajAutomtaion <raj116629@gmail.com>
2026-08-16 22:45:09 +0530

    initial commit

---
+++ playwright.config.js
@@ -0,0 +1,27 @@
+// @ts-check
+import { chromium, defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+const config = ({
+  testDir: './tests',
+  reporter:'html',
+  
+  use: {
+    
+  browserName:'chromium'
+
+  },
+});
+
+module.exports = config
+
---
+++ tests/UIBasictest.spec.js
@@ -0,0 +1,20 @@
+const {test}= require('@playwright/test')
+
+
+//javascript is asyncronus that means here the code wont be executed in sequential manner 
+test('First Playwright Test', async ({browser}) =>   // test annotation coming from playwright dependencies
+{
+
+    //chrome - plugins/cookies
+const context =await browser.newContext(); // till now only browser instance is open
+const page =await context.newPage();  // which creates new page to automate
+await page.goto("https://rahulshettyacademy.com/practice");
+} );
+
+test('Page Playwright Test', async ({page}) =>   // test annotation coming from playwright dependencies
+
+{
+
+await page.goto("https://google.com");
+
+} );
