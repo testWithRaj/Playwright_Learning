d078a284559cef5e3fa41b1d1f1eac266cd4a726
rajAutomtaion <raj116629@gmail.com>
2026-08-21 22:26:32 +0530

    adding checkbox code

---
+++ tests/checkbox.spec.js
@@ -0,0 +1,17 @@
+const {test,expect}= require('@playwright/test')
+
+test('checkbox control', async ({page}) =>   // test annotation coming from playwright dependencies
+
+{
+
+await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+console.log(await page.title());
+
+// two methods > type and fill >> type is deprecated so please use fill method
+await page.locator("input#username").fill("rahulshettyacademy");
+await page.locator("input#password").fill("Learning@830$3mK2");
+await page.locator(".radiotextsty").last();
+await page.pause();
+
+
+} )
