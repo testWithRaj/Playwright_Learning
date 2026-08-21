179338ca6c42685e57c9b43c11cb84ebe24cfe85
rajAutomtaion <raj116629@gmail.com>
2026-08-21 22:29:20 +0530

    adding more

--- tests/checkbox.spec.js
+++ tests/checkbox.spec.js
@@ -10,7 +10,8 @@ console.log(await page.title());
 // two methods > type and fill >> type is deprecated so please use fill method
 await page.locator("input#username").fill("rahulshettyacademy");
 await page.locator("input#password").fill("Learning@830$3mK2");
-await page.locator(".radiotextsty").last();
+await page.locator(".radiotextsty").last().click();
+await page.locator("#okayBtn").click();
 await page.pause();
 
 
