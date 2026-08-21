ddb85511ff90444e02d52007aa046dc3383fd46b
rajAutomtaion <raj116629@gmail.com>
2026-08-21 22:05:52 +0530

    adding static dropdown

---
+++ tests/StaticDropdown.spec.js
@@ -0,0 +1,15 @@
+const {test,expect}= require('@playwright/test')
+
+test('Test Login page using locator', async ({page}) =>   // test annotation coming from playwright dependencies
+
+{
+
+await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+console.log(await page.title());
+
+// two methods > type and fill >> type is deprecated so please use fill method
+await page.locator("input#username").fill("rahulshettyacademy");
+await page.locator("input#password").fill("Learning@830$3mK2");
+
+
+} )
