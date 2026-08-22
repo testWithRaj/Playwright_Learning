8559aaa9e2e89bba8782c3e096ae92ca23bcf0d3
rajAutomtaion <raj116629@gmail.com>
2026-08-22 23:23:12 +0530

    assertion

--- tests/StaticDropdown.spec.js
+++ tests/StaticDropdown.spec.js
@@ -9,7 +9,7 @@ console.log(await page.title());
 
 // two methods > type and fill >> type is deprecated so please use fill method
 await page.locator("input#username").fill("rahulshettyacademy");
-await page.locator("input#password").fill("Learning@830$3mK2");
+await page.locator("input#password").fill("Learning@830$3mK224");
 const dropdown=await page.locator("select.form-control");
 await dropdown.selectOption("consult");
 await page.pause();
